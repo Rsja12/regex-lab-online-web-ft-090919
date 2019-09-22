f49fb16cb7bef3f150b16de122d24ef9e779406f
Alfonso Socarras <asocarras10@gmail.com>
2019-09-22 20:07:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,21 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/^[aeiou]|[AEIOU]/)
+    true 
+  else 
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+\.$/) ? true : false 
 end
 
 def valid_phone_number?(phone)
